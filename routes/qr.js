83b59caa3d68996fe4dcbb3bc35f6f56507992f0
Nouh Rifai <nouhrifai@MacBook-Pro-de-Nouh.local>
2021-10-18 17:15:51 +0200

    last version

--- routes/qr.js
+++ routes/qr.js
@@ -6,27 +6,24 @@ const dialogflow = require('@google-cloud/dialogflow');
 // Instantiates the Intent Client
 const intentsClient = new dialogflow.IntentsClient();
 
-const projectId = 'chatbot-abb-omxw';
+const projectId = 'chatbot0abb';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET home page. */
+/* GET Req to add an intent. */
 router.get('/add', function(req, res, next) {
-  const displayName = 'test';
-  const trainingPhrasesParts = [];
-  trainingPhrasesParts.push(req.query.question);
-  const messageTexts = [req.query.reponse];
+  const displayName = req.query.title;
+  console.log(displayName);
+  const trainingPhrasesParts = req.query.question;
+  const messageTexts = req.query.response;
   createIntent(projectId, displayName, trainingPhrasesParts, messageTexts);
   res.header("Access-Control-Allow-Origin", "*");
   res.send({ message: 'OK' });
 });
 
-/**
- * TODO(developer): Uncomment the following lines before running the sample.
- */
 
 async function createIntent(projectId, displayName, trainingPhrasesParts, messageTexts) {
   // Construct request
@@ -74,9 +71,93 @@ async function createIntent(projectId, displayName, trainingPhrasesParts, messag
   console.log(`Intent ${response.name} created`);
 }
 
+/* GET Req to list intents. */
+router.get('/list', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  const agentPath = intentsClient.projectAgentPath(projectId);
+
+  //constructing list intents request
+  const listIntentsRequest = {
+    parent: agentPath,
+    intent_view: 'INTENT_VIEW_FULL'
+  };
+  intentsClient.listIntents(listIntentsRequest).then(response => {
+    res.send({ intents: response[0] });
+  })
+
+});
 
 
+/* GET Req to get a specific intent. */
+router.get('/get', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  const nameIntent = req.query.name;
+  
+//constructing get intent request
+  const getIntentRequest = {
+    name: nameIntent,
+    intentView: 'INTENT_VIEW_FULL'
+  };
+  let listQuestions = [];
+  let listResponses = [];
+  intentsClient.getIntent(getIntentRequest).then(response => {
+    //constructing training phrases array
+    response[0].trainingPhrases.forEach((tp)=> {
+      listQuestions.push(tp.parts[0].text);
+    })
 
+    //constructing responses array
+    response[0].messages.forEach(r => {
+      listResponses.push(r.text.text);
+    })
 
+    res.send({ questions: listQuestions, responses: listResponses });
+  })
+});
+
+/* GET Req to update a specific intent. */
+router.get('/update', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+
+  const intentName = req.query.intentName;
+  const intentDisplayName = req.query.intentDisplayName;
+  const question = req.query.question;
+  const response = req.query.response;
+  
+  //constructing delete intent request
+  const deleteIntentRequest = {
+    name: intentName,
+  };
+  //delete the old intent
+  intentsClient.deleteIntent(deleteIntentRequest).then(()=>{
+
+    //create the new intent
+    createIntent(projectId, intentDisplayName, question, response);
+
+  });
+
+  
+
+});
+
+/* GET Req to delete a specific intent. */
+router.get('/delete', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+
+  const intentName = req.query.intentName;
+  
+  
+  //constructing delete intent request
+  const deleteIntentRequest = {
+    name: intentName,
+  };
+  //delete the old intent
+  intentsClient.deleteIntent(deleteIntentRequest).then(()=>{
+    res.send({message : "Intent "+intentName+" Deleted Succesfully!"})
+  });
+
+  
+
+});
 
 module.exports = router;
